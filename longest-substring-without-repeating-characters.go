7ab8a0a2152aea94650bf1a55fb129495a8ec42b
Max <seemaxagain@outlook.com>
2019-07-18 08:11:05 +0800

    update longest-substring-without-repeating-characters.go

--- longest-substring-without-repeating-characters.go
+++ longest-substring-without-repeating-characters.go
@@ -1,19 +1,27 @@
-func lengthOfLongestSubstring(s string) int {
 
+func lengthOfLongestSubstring(s string) int {
+	// location[s[i]] == j 表示：
+	// s中第i个字符串，上次出现在s的j位置，所以，在s[j+1:i]中没有s[i]
+	// location[s[i]] == -1 表示： s[i] 在s中第一次出现
 	location := [256]int{}
-	maxLen := 0
 	for i := range location {
 		location[i] = -1
 	}
-	for i, j := 0, 0; j < len(s); j++ {
-		//怎么判断重复的。 location 数组的初始值都是0， 如果location[j] >0 ,说明这个重复了， 重新开始计数
-		//如果重复了， 重新求值， 如果不重复求长度
-		if location[s[j]] >= i {
-			i = location[s[j]] + 1
-		} else if j-i+1 > maxLen {
-			maxLen = j - i + 1
+
+	left := 0
+	maxLen := 0
+	for i := 0; i < len(s); i++ {
+		//如何判断s[left:i]是否重复出现过。
+		//两种情况一种是从未出现过 location[s[i]] = -1 如果大于0 ， 说明之前出现过
+		//但是还是要考虑， 窗口是滑动的， 这个字符串的开始不是0，
+		//而是最后一个不重复的开始
+		if location[s[i]] >= left {
+			left = location[s[i]] + 1
+		} else if i-left+1 > maxLen {
+			maxLen = i - left + 1
 		}
-		location[s[j]] = j
+		//记录每个字符出现的位置
+		location[s[i]] = i
 	}
 	return maxLen
 }
