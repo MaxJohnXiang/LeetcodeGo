70b78394c0628bd96a9f11f794521015e49d5244
Max <seemaxagain@outlook.com>
2019-07-22 22:17:27 +0800

    update subsets.go

--- subsets.go
+++ subsets.go
@@ -1,17 +1,17 @@
-
 func subsets(nums []int) [][]int {
-	res := make([][]int, 0)
-	dfs(&res, nums, []int{}, 0)
-	return res
-}
 
-func dfs(res *[][]int, nums []int, set []int, start int) {
-	temp := make([]int, len(set))
-	copy(temp, set)
-	*res = append(*res, temp)
-	for i := start; i < len(nums); i++ {
-		set = append(set, nums[i])
-		dfs(res, nums, set, i+1)
-		set = set[:len(set)-1]
+	res := make([][]int, 0)
+	var dfs func([]int, int)
+	dfs = func(set []int, start int) {
+		temp := make([]int, len(set))
+		copy(temp, set)
+		res = append(res, temp)
+		for i := start; i < len(nums); i++ {
+			temp = append(temp, nums[i])
+			dfs(temp, i+1)
+			temp = temp[:len(temp)-1]
+		}
 	}
+	dfs([]int{}, 0)
+	return res
 }
