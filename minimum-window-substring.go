932cbea522a20f0a40fd40d0f2eba690da09bb18
Max <seemaxagain@outlook.com>
2019-07-25 17:24:30 +0800

    update minimum-window-substring.go

--- minimum-window-substring.go
+++ minimum-window-substring.go
@@ -1,28 +1,28 @@
+// package problem0076
 
 func minWindow(s string, t string) string {
 	have := make(map[rune]int)
 	need := make(map[rune]int)
 
 	for _, v := range t {
-		need[v]++
+		need[rune(v)]++
 	}
-
+	count := 0
 	min := len(s) + 1
 	res := ""
-	for l, r, count := 0, 0, 0; r < len(s); r++ {
+
+	for l, r := 0, 0; r < len(s); r++ {
 		ch := rune(s[r])
 		have[ch]++
-		if _, ok := need[ch]; ok && have[ch] == need[ch] {
+		if have[ch] == need[ch] {
 			count++
 		}
-
-		for count == len(need) && l <= r {
+		for count >= len(need) && l <= r {
+			ch := rune(s[l])
 			if r-l+1 < min {
 				min = r - l + 1
 				res = s[l : r+1]
 			}
-
-			ch = rune(s[l])
 			have[ch]--
 			l++
 			if have[ch] < need[ch] {
