c4a3bfb841c4667fe40d41d937552805a20f4de4
Max <seemaxagain@outlook.com>
2019-07-26 18:20:15 +0800

    update longest-palindromic-subsequence.go

---
+++ longest-palindromic-subsequence.go
@@ -0,0 +1,30 @@
+// package problem0516
+
+func longestPalindromeSubseq(s string) int {
+	n := len(s)
+
+	// dp[i][j] 表示 s[i:j+1] 中最长的回文子字符串
+	dp := make([][]int, n)
+	for i := range dp {
+		dp[i] = make([]int, n)
+		dp[i][i] = 1
+	}
+
+	for i := n; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				dp[i][j] = dp[i+1][j-1] + 2
+			} else {
+				dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[0][n-1]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
