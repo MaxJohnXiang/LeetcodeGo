0b28f8d29e8124a2724713378f480b5e7c1751cb
Max <seemaxagain@outlook.com>
2019-07-26 16:24:32 +0800

    update longest-palindromic-substring.go

--- longest-palindromic-substring.go
+++ longest-palindromic-substring.go
@@ -7,10 +7,12 @@ func longestPalindrome(s string) string {
 		dp = append(dp, make([]bool, len(s)))
 	}
 	res := ""
+	maxLen := 0
 	for i := n; i >= 0; i-- {
 		for j := i; j < n; j++ {
 			dp[i][j] = s[i] == s[j] && (j-i <= 2 || dp[i+1][j-1])
-			if dp[i][j] && (res == "" || j-i+1 > len(res)) {
+			if dp[i][j] && j-i+1 >= maxLen {
+				maxLen = j - i + 1
 				res = s[i : j+1]
 			}
 		}
