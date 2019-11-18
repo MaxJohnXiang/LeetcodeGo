07248b1407682353361594947d12c2af36255e2d
Max <seemaxagain@outlook.com>
2019-11-18 23:16:46 +0800

    update reverse-integer.go

--- reverse-integer.go
+++ reverse-integer.go
@@ -1,27 +1,18 @@
-import "math"
-
-//import "math"
-
-// import "math"
-
 // package problem0007
-
-// import "math"
+//
+// import (
+// 	"math"
+// )
 
 func reverse(x int) int {
-	sign := 1
-	if x < 0 {
-		sign = sign * -1
-		x = x * sign
+
+	res := 0
+	for x != 0 {
+		res = res*10 + x%10
+		if res > math.MaxInt32 || res < math.MinInt32 {
+			return 0
+		}
+		x = x / 10
 	}
-	ans := 0
-	for x > 0 {
-		temp := x % 10
-		ans = ans*10 + temp
-		x /= 10
-	}
-	if ans > math.MaxInt32 || ans < math.MinInt32 {
-		return 0
-	}
-	return ans * sign
+	return res
 }
