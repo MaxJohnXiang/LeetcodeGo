c81fa0bfa757609f7db67ddd2c182f1fb7a4cfe2
Max <seemaxagain@outlook.com>
2020-02-12 15:37:30 +0800

    update longest-palindromic-substring.go

--- longest-palindromic-substring.go
+++ longest-palindromic-substring.go
@@ -6,35 +6,26 @@ func longestPalindrome(s string) string {
 		//肯定是回文
 		return s
 	}
-
-	res := ""
-	n := len(s)
-
-	dp := make([][]bool, n)
-
-	// for i := 0; i < n; i++ {
-	// 	dp = append(dp, make([]bool, n))
-	// }
-	for i := 0; i < n; i++ {
-		dp[i] = make([]bool, n)
-	}
-	//声明数组
-	maxLen := 0
-	for i := n - 1; i >= 0; i-- {
-		for j := i; j < n; j++ {
-			//有两种条件是回文， 第一种字符串长度为1 是回文
-			//2. 字符串长度为2 ， 两个字母都相等就为回文
-			//3, 字符串长度大于2, 最外层字母相等，并且里面是回文
-			if s[i] == s[j] && (j-i <= 2 || dp[i+1][j-1]) {
-				dp[i][j] = true
-			}
-
-			if dp[i][j] && j-i+1 >= maxLen {
-				res = s[i : j+1]
-				maxLen = j - i + 1
-			}
-		}
+	var max string
+	for i := 0; i < len(s); i++ {
+		max = isPalindrome(s, i, i, max)
+		max = isPalindrome(s, i, i+1, max)
 	}
 
-	return res
+	return max
+}
+
+//找到最长的回文
+func isPalindrome(s string, i int, j int, max string) string {
+	var sub string
+	for (i >= 0 && j < len(s)) && s[i] == s[j] {
+		sub = s[i : j+1]
+		i--
+		j++
+	}
+	if len(sub) > len(max) {
+		max = sub
+	}
+
+	return max
 }
