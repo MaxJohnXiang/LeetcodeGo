dd8d3f47b23d2ad0398055145b884a5f192a7bbb
Max <seemaxagain@outlook.com>
2020-02-13 18:13:12 +0800

    update longest-palindromic-substring.go

--- longest-palindromic-substring.go
+++ longest-palindromic-substring.go
@@ -5,22 +5,26 @@ func longestPalindrome(s string) string {
 		return s
 	}
 
-	dp := make([][]bool, 0)
-
+	var max string
 	for i := 0; i < len(s); i++ {
-		dp = append(dp, make([]bool, len(s)))
+		max = isPlidrome(s, i, i, max)
+		max = isPlidrome(s, i, i+1, max)
+	}
+	return max
+}
+
+func isPlidrome(s string, i, j int, max string) string {
+
+	var sub string
+	for i >= 0 && j < len(s) && s[i] == s[j] {
+		sub = s[i : j+1]
+		i--
+		j++
 	}
 
-	maxLen := 0
-	res := ""
-	for i := len(s) - 1; i >= 0; i-- {
-		for j := i; j < len(s); j++ {
-			dp[i][j] = s[i] == s[j] && (j-i <= 2 || dp[i+1][j-1])
-			if dp[i][j] && j-i+1 >= maxLen {
-				maxLen = j - i + 1
-				res = s[i : j+1]
-			}
-		}
+	if len(sub) > len(max) {
+		max = sub
 	}
-	return res
+
+	return max
 }
