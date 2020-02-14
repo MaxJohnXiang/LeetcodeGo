1b2e7ffd711cf52d38f1aefda9b13885d3784372
Max <seemaxagain@outlook.com>
2020-02-14 18:36:02 +0800

    update 3sum.go

--- 3sum.go
+++ 3sum.go
@@ -1,21 +1,22 @@
 // package problem0015
-//
+
 // import "sort"
 
 func threeSum(nums []int) [][]int {
-	// 排序后，可以按规律查找
+	//双指针法
+	//sort array first
+
 	sort.Ints(nums)
-	res := [][]int{}
-	//需要保证最少有三个数字， 如果少于三个数， 则没有结果
+
+	res := make([][]int, 0)
 	for i := 0; i < len(nums)-2; i++ {
 		lo := i + 1
 		hi := len(nums) - 1
-		sum := -nums[i]
 		if i == 0 || (i > 0 && nums[i] != nums[i-1]) {
+
 			for lo < hi {
-				//这个时候lo , hi 都可能越界
-				if nums[lo]+nums[hi] == sum {
-					res = append(res, []int{nums[i], nums[lo], nums[hi]})
+				if nums[lo]+nums[hi]+nums[i] == 0 {
+					res = append(res, []int{nums[lo], nums[hi], nums[i]})
 					for lo < hi && nums[lo] == nums[lo+1] {
 						lo++
 					}
@@ -24,10 +25,10 @@ func threeSum(nums []int) [][]int {
 					}
 					lo++
 					hi--
-				} else if nums[lo]+nums[hi] < sum {
-					lo++
-				} else {
+				} else if nums[lo]+nums[hi]+nums[i] > 0 {
 					hi--
+				} else {
+					lo++
 				}
 			}
 		}
