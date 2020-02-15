ea063a19efb3af81cbcd7be8d0359210a2fc849c
Max <seemaxagain@outlook.com>
2020-02-15 09:59:31 +0800

    update 3sum-closest.go

--- 3sum-closest.go
+++ 3sum-closest.go
@@ -7,23 +7,26 @@
 func threeSumClosest(nums []int, target int) int {
 	// 排序后，可以按规律查找
 	sort.Ints(nums)
-	result := nums[0] + nums[1] + nums[len(nums)-1]
-	for i := 0; i < len(nums); i++ {
+
+	max := nums[0] + nums[1] + nums[2]
+	for i := 0; i < len(nums)-2; i++ {
 		lo, hi := i+1, len(nums)-1
 		for lo < hi {
-			sum := nums[lo] + nums[hi] + nums[i]
+			sum := nums[i] + nums[lo] + nums[hi]
 
 			if sum < target {
 				lo++
 			} else {
 				hi--
 			}
-			if abs(sum-target) < abs(result-target) {
-				result = sum
+
+			if abs(sum-target) < abs(max-target) {
+				max = sum
 			}
 		}
 	}
-	return result
+
+	return max
 }
 
 func abs(a int) int {
