dcca73d32632a617951377c9d8b0d65f4f64928d
Max <seemaxagain@outlook.com>
2020-03-04 22:56:05 +0800

    update longest-substring-without-repeating-characters.go

--- longest-substring-without-repeating-characters.go
+++ longest-substring-without-repeating-characters.go
@@ -1,28 +1,23 @@
 // package problem0003
 
 func lengthOfLongestSubstring(s string) int {
-	// location[s[i]] == j 表示：
-	// s中第i个字符串，上次出现在s的j位置，所以，在s[j+1:i]中没有s[i]
-	// location[s[i]] == -1 表示： s[i] 在s中第一次出现
 	location := make([]int, 128)
-
-	//init location
 	for i := range location {
 		location[i] = -1
 	}
 
 	start := 0
 	maxLen := 0
+	//abbc
 	for i := 0; i < len(s); i++ {
-		//start 是重复的地方
+		//滑动窗口
+		//start 记录没有重复的的开始区间， 一旦出现重复， 后移动格子一
 		if location[s[i]] >= start {
 			start = location[s[i]] + 1
-		}
-		if i-start+1 >= maxLen {
+		} else if i-start+1 > maxLen {
 			maxLen = i - start + 1
 		}
 		location[s[i]] = i
 	}
-
 	return maxLen
 }
