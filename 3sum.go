88de4438aac528c34ae02d0ad8f8a282ad91b2c5
Max <seemaxagain@outlook.com>
2020-03-09 15:23:26 +0800

    update 3sum.go

--- 3sum.go
+++ 3sum.go
@@ -1,5 +1,6 @@
 // package problem0015
 //
+// //
 // import "sort"
 
 func threeSum(nums []int) [][]int {
@@ -10,25 +11,26 @@ func threeSum(nums []int) [][]int {
 
 	res := make([][]int, 0)
 	for i := 0; i < len(nums)-2; i++ {
-		if i == 0 || (i > 0 && nums[i] != nums[i-1]) {
-			lo, hi := i+1, len(nums)-1
-			for lo < hi {
-				sum := nums[i] + nums[lo] + nums[hi]
-				if sum == 0 {
-					res = append(res, []int{nums[i], nums[lo], nums[hi]})
-					for lo < hi && nums[lo] == nums[lo+1] {
-						lo++
-					}
-					for lo < hi && nums[hi] == nums[hi-1] {
-						hi--
-					}
-					lo++
-					hi--
-				} else if sum > 0 {
-					hi--
-				} else {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		lo, hi := i+1, len(nums)-1
+		for lo < hi {
+			sum := nums[i] + nums[lo] + nums[hi]
+			if sum == 0 {
+				res = append(res, []int{nums[i], nums[lo], nums[hi]})
+				for lo < hi && nums[lo] == nums[lo+1] {
 					lo++
 				}
+				for lo < hi && nums[hi] == nums[hi-1] {
+					hi--
+				}
+				lo++
+				hi--
+			} else if sum > 0 {
+				hi--
+			} else {
+				lo++
 			}
 		}
 	}
