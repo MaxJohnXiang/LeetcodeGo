6dc2a97ffe1b23f708a41eddb76c08ba960c3e04
Max <seemaxagain@outlook.com>
2020-03-11 18:35:16 +0800

    update merge-two-sorted-lists.go

--- merge-two-sorted-lists.go
+++ merge-two-sorted-lists.go
@@ -1,14 +1,14 @@
 // package problem0021
-//
-// /**
-//  * Definition for singly-linked list.
-//  * type ListNode struct {
-//  *     Val int
-//  *     Next *ListNode
-//  * }
-//  */
-//
-// // ListNode 是链接节点
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// ListNode 是链接节点
 // type ListNode struct {
 // 	Val  int
 // 	Next *ListNode
@@ -16,22 +16,21 @@
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	//corner case
-
 	if l1 == nil {
 		return l2
 	}
 	if l2 == nil {
 		return l1
 	}
-
 	l3 := &ListNode{}
 	newHead := l3
+
 	for l1 != nil && l2 != nil {
 		if l1.Val < l2.Val {
-			l3.Next = &ListNode{Val: l1.Val}
+			l3.Next = l1
 			l1 = l1.Next
 		} else {
-			l3.Next = &ListNode{Val: l2.Val}
+			l3.Next = l2
 			l2 = l2.Next
 		}
 		l3 = l3.Next
@@ -44,6 +43,5 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l2 != nil {
 		l3.Next = l2
 	}
-
 	return newHead.Next
 }
