fa819e416fb9b27f03fe37b92ee05597c559157b
Max <seemaxagain@outlook.com>
2020-03-26 11:42:31 +0800

    update remove-nth-node-from-end-of-list.go

--- remove-nth-node-from-end-of-list.go
+++ remove-nth-node-from-end-of-list.go
@@ -1,30 +1,28 @@
 // package problem0019
-//
-// /*
-//  * Definition for singly-linked list.
-//  * type ListNode struct {
-//  *     Val int
-//  *     Next *ListNode
-//  * }
-//  */
-//
-// // ListNode 是节点
+
+/*
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// ListNode 是节点
 // type ListNode struct {
 // 	Val  int
 // 	Next *ListNode
 // }
-
+//
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-
 	start := &ListNode{}
 
+	start.Next = head
 	slow, fast := start, start
 
-	start.Next = head
+	//move n
 
-	//fast move n fast
-
-	//n always valid
+	//move n step
 	for i := 1; i <= n+1; i++ {
 		fast = fast.Next
 	}
@@ -35,6 +33,5 @@ func removeNthFromEnd(head *ListNode, n int) *ListNode {
 	}
 
 	slow.Next = slow.Next.Next
-
 	return start.Next
 }
