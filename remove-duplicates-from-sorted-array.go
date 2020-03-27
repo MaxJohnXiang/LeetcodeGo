c4d96922e713076fc8933d64acc3f00565fd138c
Max <seemaxagain@outlook.com>
2020-03-27 11:38:16 +0800

    update remove-duplicates-from-sorted-array.go

--- remove-duplicates-from-sorted-array.go
+++ remove-duplicates-from-sorted-array.go
@@ -1,15 +1,17 @@
+// package problem0026
 
 func removeDuplicates(a []int) int {
-	//这个数组是有序的
-	//有两个指针， 一个记录正在遍历的指针， 一个记录不重复的指针
-	left, right := 0, 1
-	for right < len(a) {
-		//重复了
-		if a[right] != a[left] {
-			left++
-		}
-		a[left] = a[right]
-		right++
+	if len(a) < 1 {
+		return 0
 	}
+	left, right, size := 0, 1, len(a)
+	for ; right < size; right++ {
+		if a[right] == a[left] {
+			continue
+		}
+		left++
+		a[left], a[right] = a[right], a[left]
+	}
+
 	return left + 1
 }
