c4b1d9f44a6cb47a780c74c20275f3dc43c66b6a
Max <seemaxagain@outlook.com>
2020-06-02 19:27:09 +0800

    update 3sum.go

--- 3sum.go
+++ 3sum.go
@@ -1,36 +1,38 @@
 // package problem0015
-//
-// //
+
 // import "sort"
 
+//
+// //
+
 func threeSum(nums []int) [][]int {
-	//双指针法
-	//sort array first
-
-	sort.Ints(nums)
-
 	res := make([][]int, 0)
+	//排序是前提
+	sort.Ints(nums)
 	for i := 0; i < len(nums)-2; i++ {
+		//处理重复
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
-		lo, hi := i+1, len(nums)-1
-		for lo < hi {
-			sum := nums[i] + nums[lo] + nums[hi]
-			if sum == 0 {
-				res = append(res, []int{nums[i], nums[lo], nums[hi]})
-				for lo < hi && nums[lo] == nums[lo+1] {
-					lo++
-				}
-				for lo < hi && nums[hi] == nums[hi-1] {
-					hi--
-				}
-				lo++
-				hi--
-			} else if sum > 0 {
-				hi--
+		j, k := i+1, len(nums)-1
+		for j < k {
+			sum := nums[i] + nums[j] + nums[k]
+			//大了 k
+			if sum > 0 {
+				k--
+			} else if sum < 0 {
+				j++
 			} else {
-				lo++
+				res = append(res, []int{nums[i], nums[j], nums[k]})
+				//重点要处理重复的情况.
+				for j < k && nums[j] == nums[j+1] {
+					j++
+				}
+				for j < k && nums[k] == nums[k-1] {
+					k--
+				}
+				j++
+				k--
 			}
 		}
 	}
