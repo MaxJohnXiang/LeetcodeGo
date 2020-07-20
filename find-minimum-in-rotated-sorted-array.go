9cd9f0100ac4f37190bee23f44bcf250c441ecb7
Max <seemaxagain@outlook.com>
2020-07-20 20:07:17 +0800

    update find-minimum-in-rotated-sorted-array.go

--- find-minimum-in-rotated-sorted-array.go
+++ find-minimum-in-rotated-sorted-array.go
@@ -3,20 +3,14 @@
 func findMin(a []int) int {
 	nums := a
 	left, right, mid := 0, len(a)-1, 0
-
 	for left <= right {
-		mid = left + (right-left)/2
-		//[mid, right] rotated
 		if nums[left] <= nums[right] {
 			return nums[left]
 		}
-
+		mid = left + (right-left)/2
 		if nums[left] <= nums[mid] {
-			//[left, mid ] 连续
-			//肯定不在这里
 			left = mid + 1
 		} else {
-			//在[left,mid]有最小值, 找到这个最小的递增开始.
 			right = mid
 		}
 	}
